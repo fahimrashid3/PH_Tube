86c706e99c4afe20825fa2aa0e7bbc6a38cebfd4
Your Name <fahimrashid350@gmail.com>
2024-05-01 03:24:37 +0600

    category btn added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-console.log("fuck you bro");
+const categoryLoading = async () => {
+  const res = await fetch(
+    ` https://openapi.programming-hero.com/api/videos/categories`
+  );
+  const categoryList = await res.json();
+  const categories = categoryList.data;
+
+  //   create dynamic button
+  categories.forEach((data) => {
+    const categoryId = data.category_id;
+    const categoryBtn = document.createElement("div");
+    categoryBtn.innerHTML = `
+    <button class="btn btn-outline btn-error">${data.category}</button>
+
+    `;
+    const categoryBtnContainer = document.getElementById(
+      "category_btn_container"
+    );
+    categoryBtnContainer.appendChild(categoryBtn);
+
+    createVideField(categoryId);
+  });
+};
+categoryLoading();
+
+const createVideField = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const videoList = await res.json();
+  const video = videoList.data;
+  console.log(video);
+};
