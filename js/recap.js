5563a315dbac2457f9b83e03aa092f0ba90271b5
Your Name <fahimrashid350@gmail.com>
2024-05-02 02:22:46 +0600

    no video found section add

--- js/recap.js
+++ js/recap.js
@@ -4,25 +4,48 @@ const loadVideo = async (id = 1000) => {
   );
   const videoData = await res.json();
   const videoCart = videoData.data;
-  console.log(videoCart);
+  const status = videoData.status;
+  // console.log(status);
   // get the video field and set inner html
   const videField = document.getElementById("video_field");
   videField.innerHTML = "";
 
-  videoCart.forEach((video) => {
-    // create a div
-    const phoneCard = document.createElement("div");
-    phoneCard.classList = `card bg-base-100 shadow-xl mt-10`;
-    phoneCard.innerHTML = `
-        <figure><img src=${video.thumbnail} alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="text-center font-bold text-2xl">${video.title}</h2>
-          <p class="text-center">${video.title}</p>
-         
-        </div> `;
-    videField.appendChild(phoneCard);
-  });
+  if (status) {
+    videoCart.forEach((video) => {
+      // create a div
+      const newVideoCart = document.createElement("div");
+      newVideoCart.classList = `card bg-base-100 shadow-xl mt-10`;
+      newVideoCart.innerHTML = `
+        <figure><img class="w-80 h-52" src=${video.thumbnail} alt="Thumbnail" /></figure>
+        <div class="flex mt-2 ml-10 ">
+          <div class="mt-5 ">
+            <img class="h-10 w-10 rounded-full " src=${video.authors[0].profile_picture} alt="Thumbnail" />
+          </div>
+          <div class="card-body">
+            <h2 class="text-left font-bold text-xl">${video.title}</h2>
+            <div>
+              <p class="text-left">${video.authors[0].profile_name}</p>
+
+            </div>
+            <p class="text-left text-gray-300">${video.others.views}</p>
+
+          </div> 
+        </div>
+
+        `;
+      videField.appendChild(newVideoCart);
+    });
+  } else {
+    const errorField = document.createElement("div");
+    errorField.innerHTML = `
+    <img class=" mx-auto mt-20 h-full w-full" src="Icon.png" alt="Not found">
+
+    `;
+    videField.appendChild(errorField);
+  }
 };
+
+// "profile_name
 
 const categoryLoading = async () => {
   const res = await fetch(
@@ -38,6 +61,7 @@ const categoryLoading = async () => {
     <button class="btn btn-outline btn-error" onclick="loadVideo(${categoryId} )" >${data.category}</button>
 
     `;
+
     const categoryBtnContainer = document.getElementById(
       "category_btn_container"
     );
